9446671343245521f2bb26f73e90f7d4842f2cc1
Sam-mariehamn <83921244+Sam-mariehamn@users.noreply.github.com>
2021-06-14 19:41:57 +0300

    Update lab1_3.py

--- Task 1/The one where money need to be transported/lab1_3.py
+++ Task 1/The one where money need to be transported/lab1_3.py
@@ -1,1 +1,53 @@
+#Declare variables
 
+bigCount = 0
+mediumCount = 0
+smallCount = 0
+
+bigVolume = 80
+mediumVolume = 50
+smallVolume = 20
+
+bigValue = 60000
+mediumValue = 30000
+smallValue = 10000
+
+totalValue = 0
+
+#Print welcome message
+
+print("\nWelcome to the Money Bag Transport Calculator (M.B.T.C)")
+print("-------------------------------------------------------\n")
+
+#While loop to make sure we get a valid input from the user
+
+while True:
+  truckSize = input("What is the volume of the truck (>=100L) >> ")
+  if not truckSize.isdigit() or int(truckSize) < 100:
+    print("\nInvalid input, try again\n")
+  else:
+    truckSize = int(truckSize)
+    break
+
+#Calculate how many of each bag fits into the truck and the total value
+
+bigCount = int(truckSize / bigVolume)
+truckSize = truckSize - bigCount * bigVolume
+
+mediumCount = int(truckSize / mediumVolume)
+truckSize = truckSize - mediumCount * mediumVolume
+
+smallCount = int(truckSize / smallVolume)
+truckSize = truckSize - smallCount * smallVolume
+
+totalValue = bigCount * bigValue + mediumCount * mediumValue + smallCount * smallValue
+
+#Print the result
+
+print("Packing plan")
+print("------------")
+print(str(bigCount) + " big bags")
+print(str(mediumCount) + " medium bags")
+print(str(smallCount) + " small bags\n")
+print("Space left : " + str(truckSize) + "L")
+print("Total value: " + str(totalValue) + "kr")
