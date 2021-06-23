718b5e4988486bb184ea76fdebddf2691e9b9c5f
Sam-mariehamn <83921244+Sam-mariehamn@users.noreply.github.com>
2021-06-23 23:28:38 +0300

    Update Lab2_1.py

--- Task 2/The one with the chat logs/Lab2_1.py
+++ Task 2/The one with the chat logs/Lab2_1.py
@@ -1,8 +1,9 @@
 import sys
 
+
 def read_file(filename):
 
-    try: 
+    try:
         # Opens the file as read only
         with open(filename, 'r') as chatLog:
             # Gets all lines into a list
@@ -16,8 +17,9 @@ def read_file(filename):
             it = iter(chatLogList)
             listOfTuples = list(zip(it, it))
     # Throws an exception if the file path is wrong
-    except IOError: 
-        print("The file " + filename + " could not be found.")
+    except IOError:
+        print("Error: The file '" + filename + "' could not be found.")
+        sys.exit()
 
     return listOfTuples
 
@@ -30,16 +32,16 @@ def display_entry(name, message):
 def main():
     # Checks that the correct amount of arguemnts are given
     if len(sys.argv) == 2:
-        # Asks for whose messages to print
-        name = input("Enter a name to search for >>")        
         # Gets the list of tuples from the read_file function
         listOfTuples = read_file(sys.argv[1])
+        # Asks for whose messages to print
+        name = input("Enter a name to search for >>")
         # Checks for all messages from the given name
-        # and prints them 
+        # and prints them
         for item in listOfTuples:
             if item[0] == name:
                 display_entry(name, item[1])
-    
+
     else:
         print("Too few or too many arguments were given.")
 
