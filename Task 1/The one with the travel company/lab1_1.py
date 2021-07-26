e27c37f8cc558ab79fb8401af1f61322cb48b6e8
Sam-mariehamn <83921244+Sam-mariehamn@users.noreply.github.com>
2021-07-26 18:17:55 +0300

    Update lab1_1.py

--- Task 1/The one with the travel company/lab1_1.py
+++ Task 1/The one with the travel company/lab1_1.py
@@ -1,4 +1,4 @@
-#Declare variables
+# Declare variables
 
 budget = 500
 economy = 750
@@ -14,90 +14,108 @@ max_meals = 1
 
 chosenTicket = None
 
-#Starting point to print the options available
+# Starting point to print the options available
 
-startingPoint = "Ticket types:\n1. Budget  ( " + str(budget) + "kr)\n2. Economy ( " + str(economy) + "kr)\n3. VIP     (" + str(vip) + "kr)\n"
+startingPoint = ("Ticket types:\n1. Budget  ( "
+                 + str(budget) + "kr)\n2. Economy ( "
+                 + str(economy) + "kr)\n3. VIP     ("
+                 + str(vip) + "kr)\n")
 
 print(startingPoint)
 
-#While statement to make sure the input is a valid option
+# While statement to make sure the input is a valid option
 
 while True:
 
-  ticket = input('Input ticket type >> ')
+    ticket = input('Input ticket type >> ')
 
-  if not ticket.isdigit() or not 1 <= int(ticket) <= 3:
-    print("\nInvalid choice\n")  
-    print(startingPoint)
-  else:
-    ticket = int(ticket)
-    break
+    if not ticket.isdigit() or not 1 <= int(ticket) <= 3:
+        print("\nInvalid choice\n")
+        print(startingPoint)
+    else:
+        ticket = int(ticket)
+        break
 
 if ticket == 1:
-  chosenTicket = budget
+    chosenTicket = budget
 
 if ticket == 2:
-  chosenTicket = economy
+    chosenTicket = economy
 
 if ticket == 3:
-  chosenTicket = vip
+    chosenTicket = vip
 
-#Print the current selection of bags and meals and second selection menu
+# Print the current selection of bags and meals and
+# second selection menu
 
-print("\nCurrently you have:\n\t" + str(bag_count) + " bag(s) registered\n\t" + str(meal_count) + " meal(s) registered\n")
+print("\nCurrently you have:\n\t"
+      + str(bag_count) + " bag(s) registered\n\t"
+      + str(meal_count) + " meal(s) registered\n")
 
-secondSelection = "Here are your options:\n1. Add bag (max 1)\n2. Add meal (max 1)\n3. Remove bag\n4. Remove meal\n5. Finalize ticket\n"
+secondSelection = "Here are your options:\n1.\
+                   Add bag (max 1)\n2. Add meal (max 1)\n3. \
+                   Remove bag\n4. Remove meal\n5. Finalize ticket\n"
 
 print(secondSelection)
 
-#Implementation of second selection menu, in a while loop to make sure all entered numbers are valid. There are also checks to make sure bags or meals can't be higher than the allowed value or go to negative values.
+# Implementation of second selection menu,
+# in a while loop to make sure all entered numbers are valid.
+# There are also checks to make sure bags or meals can't be higher
+# than the allowed value or go to negative values.
 
 while True:
 
-  bagMealSelection = input("Your choice >> ")
+    bagMealSelection = input("Your choice >> ")
 
-  if not bagMealSelection.isdigit() or not 1 <= int(bagMealSelection) <= 5:
-    print("\nInvalid choice\n")  
-    print(secondSelection)
-  else:    
-    bagMealSelection = int(bagMealSelection)
+    if not bagMealSelection.isdigit() or not 1 <= int(bagMealSelection) <= 5:
+        print("\nInvalid choice\n")
+        print(secondSelection)
+    else:
+        bagMealSelection = int(bagMealSelection)
 
-    if bagMealSelection == 1:
-      bag_count += 1
-      print("\nCurrently you have:\n\t" + str(bag_count) + " bag(s) registered\n\t" + str(meal_count) + " meal(s) registered\n")
-      print(secondSelection)
-      if bag_count > max_bags:
-        print("Too many bags! Please remove a bag.\n")
+        if bagMealSelection == 1:
+            bag_count += 1
+            print("\nCurrently you have:\n\t" + str(bag_count)
+                  + " bag(s) registered\n\t" + str(meal_count)
+                  + " meal(s) registered\n")
+            print(secondSelection)
+            if bag_count > max_bags:
+                print("Too many bags! Please remove a bag.\n")
 
-    elif bagMealSelection == 2:
-      meal_count += 1
-      print("\nCurrently you have:\n\t" + str(bag_count) + " bag(s) registered\n\t" + str(meal_count) + " meal(s) registered\n")
-      print(secondSelection)
-      if meal_count > max_meals:
-        print("Too many meals! Please remove a meal\n")
+        elif bagMealSelection == 2:
+            meal_count += 1
+            print("\nCurrently you have:\n\t" + str(bag_count)
+                  + " bag(s) registered\n\t" + str(meal_count)
+                  + " meal(s) registered\n")
+            print(secondSelection)
+            if meal_count > max_meals:
+                print("Too many meals! Please remove a meal\n")
 
-    elif bagMealSelection == 3:
-      bag_count -= 1
-      if bag_count < 0:
-        bag_count = 0
-      print("\nCurrently you have:\n\t" + str(bag_count) + " bag(s) registered\n\t" + str(meal_count) + " meal(s) registered\n")
-      print(secondSelection)
+        elif bagMealSelection == 3:
+            bag_count -= 1
+            if bag_count < 0:
+                bag_count = 0
+            print("\nCurrently you have:\n\t" + str(bag_count)
+                  + " bag(s) registered\n\t" + str(meal_count)
+                  + " meal(s) registered\n")
+            print(secondSelection)
 
-    elif bagMealSelection == 4:
-      meal_count -=1
-      if meal_count < 0:
-        meal_count = 0
-      print("\nCurrently you have:\n\t" + str(bag_count) + " bag(s) registered\n\t" + str(meal_count) + " meal(s) registered\n")
-      print(secondSelection)
-    
-    elif bagMealSelection == 5:
-      if bag_count > max_bags:
-        print("\nToo many bags! Please remove a bag.\n")
+        elif bagMealSelection == 4:
+            meal_count -= 1
+            if meal_count < 0:
+                meal_count = 0
+            print("\nCurrently you have:\n\t" + str(bag_count)
+                  + " bag(s) registered\n\t" + str(meal_count)
+                  + " meal(s) registered\n")
+            print(secondSelection)
 
-      elif meal_count > max_meals:
-        print("\nToo many meals! Please remove a meal\n")
-      else:
-        break
+        elif bagMealSelection == 5:
+            if bag_count > max_bags:
+                print("\nToo many bags! Please remove a bag.\n")
+            elif meal_count > max_meals:
+                print("\nToo many meals! Please remove a meal\n")
+            else:
+                break
 
 totalBagPrice = bag_count * bag
 totalMealPrice = meal_count * meal
@@ -105,7 +123,9 @@ totalPrice = chosenTicket + totalBagPrice + totalMealPrice
 
 print("Receipt:")
 print("Ticket : " + format(chosenTicket, ">4") + "kr")
-print("Bag    : " + format(bag_count * bag, ">4") + "kr")
-print("Meal   : " + format(meal_count * meal, ">4") + "kr")
+if bag_count > 0:
+    print("Bag    : " + format(bag_count * bag, ">4") + "kr")
+if meal_count > 0:
+    print("Meal   : " + format(meal_count * meal, ">4") + "kr")
 print("        -------")
 print("Total  : " + format(totalPrice, ">4") + "kr")
