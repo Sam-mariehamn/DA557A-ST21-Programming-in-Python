9e9e7235f9818da13290e4d2a522ffefe7c12282
Sam-mariehamn <83921244+Sam-mariehamn@users.noreply.github.com>
2021-07-26 18:17:04 +0300

    Update lab1_3.py

--- Task 1/The one where money need to be transported/lab1_3.py
+++ Task 1/The one where money need to be transported/lab1_3.py
@@ -1,4 +1,4 @@
-#Declare variables
+# Declare variables
 
 bigCount = 0
 mediumCount = 0
@@ -14,38 +14,43 @@ smallValue = 10000
 
 totalValue = 0
 
-#Print welcome message
+# Print welcome message
 
 print("\nWelcome to the Money Bag Transport Calculator (M.B.T.C)")
 print("-------------------------------------------------------\n")
 
-#While loop to make sure we get a valid input from the user
+# While loop to make sure we get a valid input from the user
 
 while True:
-  truckSize = input("What is the volume of the truck (>=100L) >> ")
-  if not truckSize.isdigit() or int(truckSize) < 100:
-    print("\nInvalid input, try again\n")
-  else:
-    truckSize = int(truckSize)
-    break
+    truckSize = input("What is the volume of the truck (>=100L) >> ")
+    if not truckSize.isdigit() or int(truckSize) < 100:
+        print("\nInvalid input, try again\n")
+    else:
+        truckSize = int(truckSize)
+        break
 
-#Calculate how many of each bag fits into the truck and the total value
+# Calculate how many of each bag fits into
+# the truck and the total value
 
 while truckSize >= 80:
-  bigCount += 1
-  truckSize = truckSize - bigVolume
+    bigCount += 1
+    truckSize = truckSize - bigVolume
 
 while truckSize >= 50:
-  mediumCount += 1
-  truckSize = truckSize - mediumVolume
+    mediumCount += 1
+    truckSize = truckSize - mediumVolume
 
 while truckSize >= 20:
-  smallCount += 1
-  truckSize = truckSize - smallVolume
+    smallCount += 1
+    truckSize = truckSize - smallVolume
 
-totalValue = bigCount * bigValue + mediumCount * mediumValue + smallCount * smallValue
+totalValue = (
+                bigCount * bigValue
+                + mediumCount * mediumValue
+                + smallCount * smallValue
+)
 
-#Print the result
+# Print the result
 
 print("Packing plan")
 print("------------")
